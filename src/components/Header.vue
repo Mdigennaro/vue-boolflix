904aa8eded34d07c53c59af1589c4866cd51548e
Mdigennaro <michele.digennaro719@gmail.com>
2021-12-01 11:03:48 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  
+  <header>
+
+    <div class="h-left">
+      <img src="" alt="logo">
+    </div>
+
+    <div class="h-right">
+      <input type="text" placeholder="Ricerca film">
+      <button>search</button>
+    </div>
+
+  </header>
+
+</template>
+
+<script>
+export default {
+  nome: "Header"
+}
+</script>
+
+<style lang="scss">
+  header{
+    padding: 20px;
+    height: 80px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;    
+  }
+</style>
