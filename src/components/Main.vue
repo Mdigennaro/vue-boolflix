1a16780a49eabc9ebad976a3fa6ec89c67caa450
Mdigennaro <michele.digennaro719@gmail.com>
2021-12-11 16:52:42 +0100

    aggiunta carosello

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,12 +5,15 @@
 
     <div class="container">
 
+      <VueSlickCarousel v-bind="settings">
 
-      <Compilation 
-      v-for="item in listItem" 
-      :key="item.id" 
-      :item="item"
-      />
+        <Compilation 
+        v-for="item in listItem" 
+        :key="item.id" 
+        :item="item"
+        />
+
+      </VueSlickCarousel>  
 
     </div>
 
@@ -20,12 +23,16 @@
 
 <script>
 import Compilation from './Compilation.vue';
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
   nome: "Main",
   
   components:{
     Compilation,
+    VueSlickCarousel
   },
 
 
@@ -34,7 +41,42 @@ export default {
     section: String
   },
 
-
+  data() {
+    return {
+      settings:{
+        "dots": true,
+        "infinite": false,
+        "speed": 500,
+        "slidesToShow": 4,
+        "slidesToScroll": 4,
+        "initialSlide": 0,
+        "responsive": [{
+          "breakpoint": 1024,
+          "settings": {
+            "slidesToShow": 3,
+            "slidesToScroll": 3,
+            "infinite": true,
+            "dots": true
+          }
+        },
+        {
+          "breakpoint": 600,
+          "settings": {
+            "slidesToShow": 2,
+            "slidesToScroll": 2,
+            "initialSlide": 2
+          }
+        },
+        {
+          "breakpoint": 480,
+          "settings": {
+            "slidesToShow": 1,
+            "slidesToScroll": 1
+          }
+        }]
+      }
+    }
+  }
 }
 
 
@@ -53,17 +95,8 @@ main{
   h1{
     color: goldenrod;
     font-family: 'Roboto Condensed', sans-serif;
+    padding: 10px 0;
   }
-  
-  .container{
-    width: 100%;
-    @include flex-between;
-    justify-content: flex-start;
-    overflow: hidden;
-    padding: 10px;
-    overflow-x: auto;
-  
-  }
-  
+
 }
 </style>
